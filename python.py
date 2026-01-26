843020894616caa315ee629594bdb1ec1c0d946a
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-26 21:37:52 +0530

    write vraribles

--- python.py
+++ python.py
@@ -1,3 +1,11 @@
 # this is my first python
 first_name = "bro"
+food = "pizza"
+email = "adityasikarwarr156"
+
+
 print(first_name)
+print(f"Hello {first_name}")
+print(f"you like {food}")
+print(f"your email is {email}")
+
