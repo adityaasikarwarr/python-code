bd4d3baba3d990fd75e0c45861e3f5959955bd01
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-26 21:42:26 +0530

    wrote the floart

--- python.py
+++ python.py
@@ -1,11 +1,28 @@
 # this is my first python
-first_name = "bro"
-food = "pizza"
-email = "adityasikarwarr156"
+# first_name = "bro"
+# food = "pizza"
+# email = "adityasikarwarr156"
 
 
-print(first_name)
-print(f"Hello {first_name}")
-print(f"you like {food}")
-print(f"your email is {email}")
+# print(first_name)
+# print(f"Hello {first_name}")
+# print(f"you like {food}")
+# print(f"your email is {email}")
 
+# Integers
+age = 25
+quantity = 3
+num_students = 30
+
+print(f"YOu are {age} years old ")
+print(f"YOu are buying {quantity} items")
+print(f"YOur class has {num_students} ")
+
+# float
+price = 10.45
+gpa = 9.9
+distance = 5.5
+
+print(f"the price is {price}")
+print(f"the gpa is {gpa}")
+print(f"you ran {distance}")
