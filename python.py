4c86aab62c6591cd4732c29e7a9d987923705e9a
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-27 21:41:53 +0530

    changes  madee

--- python.py
+++ python.py
@@ -1,9 +1,12 @@
 # typecasting - changing of one to another str() , int(), float(), bool()
 
-# name = "aditya sikarwarr"
-# age = 35
-# gpa = 4.5
-# is_student = True
+name = "aditya sikarwarr"
+age = 35
+gpa = 4.5
+is_student = True
 
-# gpa = int(gpa)
-# print(gpa)
+age = str(age)
+
+
+age += '1'
+print(age)
