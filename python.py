80b79c7a5849f85d98200a3a80f591ccff970c17
adityasikawarr <adityasikarwarr156@gmail.com>
2026-01-27 21:48:31 +0530

    input learnin

--- python.py
+++ python.py
@@ -1,13 +1,18 @@
 # typecasting - changing of one to another str() , int(), float(), bool()
 
-name = "aditya sikarwarr"
-age = 35
-gpa = 4.5
-is_student = True
+# name = "aditya sikarwarr"
+# age = 35
+# gpa = 4.5
+# is_student = True
+# age = str(age)
+# name = bool(name)
+# age += '1'
+# print(name)
 
-age = str(age)
-name = bool(name)
+# input() = a function that prompts the user to enter data returns the entered data as a string
 
-
-age += '1'
-print(name)
+# input("what is your name ?")
+name = input("tell me your name")
+file_name = int(input("tell me your file number"))
+print(f"hello {name}")
+print(f"your file number is {file_name}")
