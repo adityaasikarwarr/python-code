552843169b747a6e2baff58c8c45279cb2fbb4b4
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-12 22:33:39 +0530

    changes

--- new.py
+++ new.py
@@ -2,23 +2,23 @@
 # str = input()
 # result = str.swapcase()
 # print(result)
-#!/bin/python3
+# #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
+# import math
+# import os
+# import random
+# import re
+# import sys
 
 
-if __name__ == '__main__':
-    n = int(input().strip())
-    if n % 2 != 0:
-        print("Weird")
-    elif n % 2 == 0 and 2 <= n <= 5:
-        print('Not Weird')
-    elif n % 2 == 0 and 6 <= n <= 20:
-        print('Weird')
-    else:
-        print("Not Weird")
+# if __name__ == '__main__':
+#     n = int(input().strip())
+#     if n % 2 != 0:
+#         print("Weird")
+#     elif n % 2 == 0 and 2 <= n <= 5:
+#         print('Not Weird')
+#     elif n % 2 == 0 and 6 <= n <= 20:
+#         print('Weird')
+#     else:
+#         print("Not Weird")
  
