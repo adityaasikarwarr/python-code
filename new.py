3391dc5a80b31325f2aad1cf44468743e98fc166
adityasikawarr <adityasikarwarr156@gmail.com>
2026-02-13 10:26:16 +0530

    changes

--- new.py
+++ new.py
@@ -2,7 +2,7 @@
 # for i in range(num):
 #     print(i * i)
     
-for i in range(1, int(input())+1):
-    print((10**i - 1)//9 * i)
+# for i in range(1, int(input())+1): print((10**i - 1)//9 * i)
+   
 
         
