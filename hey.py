f7a65fd5847dc996b877f959aeb6e61019d0e675
adityasikawarr <adityasikarwarr156@gmail.com>
2026-03-20 09:57:21 +0530

    wrote the code

---
+++ hey.py
@@ -0,0 +1,13 @@
+t = (1,2,3,4,5,7,8,9,10)
+print(t[3])
+print(t[-4])
+
+for i in t:
+    if i == 4:
+        print("yes exists")
+    
+print(11 in t)
+
+a = [(1,"a"), (3,"b"), (5,"c")]
+val = zip(*a)
+print(list(val))
