5145e36a45bf1104e00a2632a19ca768227e029b
adityasikawarr <adityasikarwarr156@gmail.com>
2026-03-25 08:24:44 +0530

    gave a nam to new file

--- new.py
+++
@@ -1,8 +0,0 @@
-# num = int(input())
-# for i in range(num):
-#     print(i * i)
-    
-# for i in range(1, int(input())+1): print((10**i - 1)//9 * i)
-   
-for i in range( 1, 3 ,5):
-    print("hello")
