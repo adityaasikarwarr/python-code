d6dc8dc5639a663eadcd37b99315fbb49f70d4c8
adityasikawarr <adityasikarwarr156@gmail.com>
2026-04-06 14:12:44 +0530

    changes in code

--- calc.py
+++ calc.py
@@ -18,4 +18,4 @@ age = int(input("Enter a number: "))
 if age >= 18:
     print("you are eligible for voting")
 else:
- print("you are not eligible for voting
+ print("you are not eligible for voting")
