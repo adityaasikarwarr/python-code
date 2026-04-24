5740907cbdeaf5b7b9aec107a5b64f8ad96086c6
adityasikawarr <adityasikarwarr156@gmail.com>
2026-04-24 11:55:53 +0530

    changes in code

--- new2.py
+++ new2.py
@@ -1,4 +1,19 @@
-def hey():
-    print("hey")
+class car :
+    brand = None
+    name = None
+    model = None
+    
+obj1 = car()
+obj2 = car()
 
-hey()
+print(car())
+print(obj1)
+print(obj2)
+
+obj1.brand = "tata"
+obj1.name = "punch"
+obj1.model = "2020"
+
+obj2.brand = "tata"
+obj2.name = "nexon"
+obj2.model = "2021"
