195a2d1d3e5203978088a2e0cf0be1be9641c3b6
adityasikawarr <adityasikarwarr156@gmail.com>
2026-04-24 23:01:10 +0530

    wrote code of class in python

--- new2.py
+++ new2.py
@@ -1,19 +1,61 @@
+# class car :
+#     brand = None
+#     name = None
+#     model = None
+#     color = None
+    
+# obj1 = car()
+# obj2 = car()
+
+
+# print(car())
+# print(obj1)
+# print(obj2)
+
+# obj1.brand = "tata"
+# obj1.name = "punch"
+# obj1.model = "2020"
+# obj1.color = "red"
+# print(obj1.brand)
+# print(obj1.name)
+# print(obj1.model)
+# print(obj1.color)
+
+
+# obj2.brand = "tata"
+# obj2.name = "nexon"
+# obj2.model = "2021"
+# obj2.color = "blue"
+# print(obj2.brand)
+# print(obj2.name)
+# print(obj2.model)
+# print(obj2.color)
+
 class car :
     brand = None
     name = None
     model = None
+    color = None
+
+obj = car()
+
+obj.brand = "Mercedez"
+print(obj.brand)
+obj.name = "G-wagon"
+print(obj.name)
+obj.model = 2026
+print(obj.model)
+obj.color = "black"
+print(obj.color)
+
+class aditya:
+    name = None
+    age = None
+    city = None
+    vergin = None
+    sex = "yes"
     
-obj1 = car()
-obj2 = car()
+obj = aditya()
 
-print(car())
-print(obj1)
-print(obj2)
-
-obj1.brand = "tata"
-obj1.name = "punch"
-obj1.model = "2020"
-
-obj2.brand = "tata"
-obj2.name = "nexon"
-obj2.model = "2021"
+obj.sex = "No"
+print(obj.sex)
