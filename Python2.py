1989d72de47e773664a65b4a5b2c61d2003d2d7d
adityasikawarr <adityasikarwarr156@gmail.com>
2026-04-24 11:29:05 +0530

    wrote the codes

--- Python2.py
+++ Python2.py
@@ -1,49 +1,49 @@
-# fact = 1
-# n = 5
-# while n != 0:
-#      fact = fact * n
-#      n = n - 1
-# print(fact)
+fact = 1
+n = 5
+while n != 0:
+     fact = fact * n
+     n = n - 1
+print(fact)
 
-# i = 1
-# while i <= 50:
-#      if i % i == i:
-#       print(i)
+i = 1
+while i <= 50:
+     if i % i == i:
+      print(i)
 
-# def addition(*args):
-#     print(type(args))
-#     a, b, c, d = args
-#     print(a, b, c, d)
+def addition(*args):
+    print(type(args))
+    a, b, c, d = args
+    print(a, b, c, d)
    
-# addition(10 , 20 , 30 , 40)
+addition(10 , 20 , 30 , 40)
 
-# def add(a , b,c ,d,e,f,g,h):
-#     print(a + b + c + d + e + f + g + h)
+def add(a , b,c ,d,e,f,g,h):
+    print(a + b + c + d + e + f + g + h)
 
-# add(12,667,323,13,6,8,9,99)
+add(12,667,323,13,6,8,9,99)
 
-# def info(name , section , course):
-#     print("name : " + name)
-#     print("section : " + section)
-#     print("course : " + course)
-# info(name = "Aditya" , section = "A" , course = "B.tech")
+def info(name , section , course):
+    print("name : " + name)
+    print("section : " + section)
+    print("course : " + course)
+info(name = "Aditya" , section = "A" , course = "B.tech")
 
-# def infos(**details):
-#     print(details)
-#     print(type(details))
-# infos(name = "Aditya" , section = "A" , course = "B.tech" , building = "A")
+def infos(**details):
+    print(details)
+    print(type(details))
+infos(name = "Aditya" , section = "A" , course = "B.tech" , building = "A")
 
-# def academic(**details):
-#     print(details)
-#     for i , j in details.items():
-#         print(i , j)
-# academic(name = "Aditya" , section = "A" , course = "B.tech" , building = "A")
+def academic(**details):
+    print(details)
+    for i , j in details.items():
+        print(i , j)
+academic(name = "Aditya" , section = "A" , course = "B.tech" , building = "A")
 
-# def mul(*args):
-#     a = int(input())
-#     b = int(input())
-#     c = int(input())
-#     d = int(input())
-#     e = int(input())
-#     print(a * b * c * d* e)
-# mul()
+def mul(*args):
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    e = int(input())
+    print(a * b * c * d* e)
+mul()
--- calc.py
+++ calc.py
@@ -1,17 +1,17 @@
-# operator = input("Enter the operator (+, -, *, /): ")
-# num1 = float(input("Enter the first number: "))
-# num2  = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+num1 = float(input("Enter the first number: "))
+num2  = float(input("Enter the second number: "))
 
-# if operator == "+":
-#     print(num1 + num2)
-# elif operator == "-":
-#     print(num1 - num2)
-# elif operator == "*":
-#     print(num1 * num2)
-# elif operator == "/":
-#     print(num1 / num2)
-# else:
-#     print("invalid operator")
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("invalid operator")
 
 age = int(input("Enter a number: "))
 
