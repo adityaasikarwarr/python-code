2e6360d46b3b555b15a65eb1b15fdd240f51f89c
adityasikawarr <adityasikarwarr156@gmail.com>
2026-05-13 21:57:00 +0530

    compound interest calculator

---
+++ CompoundInterest.py
@@ -0,0 +1,20 @@
+principle = 0
+rate = 0
+time= 0
+
+while principle <= 0:
+    principle = float(input("enter the principle amount :"))
+    if principle <= 0:
+        print("principle must be greater than 0")
+        
+while rate <= 0:
+    rate = float(input("enter the rate :"))
+    if rate <= 0:
+        print("rate must be greater than 0")
+        
+while time <= 0:
+    time = float(input("enter the time in years :"))
+    if time <= 0:
+        print("time must be greater than 0")
+total= principle* pow((1+rate/100), time)
+print(f"your total amount is :\t{total}")
