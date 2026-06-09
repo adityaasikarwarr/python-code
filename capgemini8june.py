37eb471fd0712ff3f631a93d6e761af5dbe5c5a3
adityasikawarr <adityasikarwarr156@gmail.com>
2026-06-09 22:53:04 +0530

    wrote the code for resturant menu item manage

--- capgemini8june.py
+++ capgemini8june.py
@@ -11,9 +11,9 @@ class Library :
         
     def show_book_details(self) :
         print("LIBRARY BOOK DETAILS : ")
-        print(self.book_name)
-        print(self.author_name)
-        print(self.price)
+        print("Book name = " , self.book_name)
+        print("Author name = " , self.author_name)
+        print("Book Price = " , self.price)
         
 book1 = Library()
 book2 = Library()
@@ -29,3 +29,43 @@ book1.show_book_details()
 
 print("SHOW BOOK DETAILS : ")
 book2.show_book_details()
+
+class FoodItems :
+    def item_details(self):
+        self.item_name = ""
+        self.item_price = 0
+        self.item_quantity = 0
+        
+    def add_food_item(self):
+        self.item_name = input("Enter your food item : ")
+        self.item_price = int(input("Enter the price : "))
+        self.item_quantity = int(input("Enter the quantity "))
+        
+    def show_item(self):
+        print("FOOF ITEM DETAIL")
+        print("Item name = " , self.item_name)
+        print("Item price = " , self.item_price)
+        print("Item quantity = " , self.item_quantity)
+        
+    def total_bill(self):
+        self.item_price = self.item_price * self.item_quantity
+        print("Total bill = ", self.item_price)
+        
+food_item1 = FoodItems()
+food_item2 = FoodItems()
+
+print("ENTER FOOD ITEM DETAILS :")
+food_item1.add_food_item()
+print("ENTER FOOD ITEM DETAILS :")
+food_item2.add_food_item()
+
+print("SHOW ITEM DETAILS :")
+food_item1.show_item()
+
+print("SHOW ITEM DETAILS :")
+food_item2.show_item()
+
+print("TOTAL BILL ITEM 1")
+food_item1.total_bill()
+print("TOTAL BILL ITEM 2")
+food_item2.total_bill()
