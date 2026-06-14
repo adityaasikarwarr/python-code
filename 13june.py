3d82a502d538bc472d33b7eb381fff5dadb46bba
adityasikawarr <adityasikarwarr156@gmail.com>
2026-06-14 22:17:26 +0530

    changes

--- 13june.py
+++ 13june.py
@@ -19,3 +19,5 @@ class MobileApp(BankApp):
         
 app = MobileApp()
 app.database()
+
+# abstract method forces child class to use some method which is implemented in parent class and their must be a abstract method in abstract class by adding ABC as argument in abstract class
