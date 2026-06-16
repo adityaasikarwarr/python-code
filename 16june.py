dc22215ecf16355e344c90aaf5f5ec9132e488dc
adityasikawarr <adityasikarwarr156@gmail.com>
2026-06-16 14:50:17 +0530

    lambda codes

--- 16june.py
+++ 16june.py
@@ -10,3 +10,19 @@ print(add(5, 3))
 n = int(input("Enter a number: "))
 check = lambda n : "even" if n % 2 == 0 else "odd"
 print(check(n))
+
+year = int(input("Enter your year : "))
+leapyear = lambda year : print("leap year") if (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)) else print("not leap year")
+leapyear(year)
+
+num1 = int(input("Enter your number 1 : "))
+num2 = int(input("Enter your number 1 : "))
+num3 = int(input("Enter your number 1 : "))
+largest1 = lambda num1 , num2 : print("num1 is greatest") if num1 > num2 else print("num2 is greater")
+largest2 = lambda num1 , num2 , num3 : max(num1 , num2 , num3)
+largest1(num1 , num2)
+print(largest2(num1 , num2 , num3))
+
+string = input("Enter your word : ")
+palindrome = lambda string : print(string[::-1])
+palindrome(string)
